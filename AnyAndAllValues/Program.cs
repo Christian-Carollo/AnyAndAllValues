d454bcdd8618cd89b9b5fd42e52d00dd0beabec0
Administrator <Administrator@DESKTOP-95RB6IG>
2023-11-23 10:30:07 +0100

    Aggiungere i file di progetto.

---
+++ AnyAndAllValues/Program.cs
@@ -0,0 +1,5 @@
+﻿List<int> lisInteri = new() { 1, 25, 11, 97, 53, 68 };
+
+Console.WriteLine("I numeri della lista sono:\n");
+
+lisInteri.Select(n => n % 2 == 0 ? $"{n}: Pari" :  $"{n}: Dispari").ToList().ForEach(n => Console.WriteLine(n));
